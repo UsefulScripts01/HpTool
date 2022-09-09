6a44747e4bf3535acd839a85c9bbde49f38bae06
Dawid Prowadzisz <dawid.prowadzisz@gmail.com>
2022-09-09 21:38:17 +0200

    a

--- HpModule.ps1
+++ HpModule.ps1
@@ -1,1 +1,12 @@
-a
+$progressPreference = "SilentlyContinue"
+
+function Get-HpModule {
+    Invoke-WebRequest -Uri "https://hpia.hpcloud.hp.com/downloads/cmsl/hp-cmsl-1.6.7.exe" -OutFile "C:\Windows\Temp\HpModule.exe"
+    Start-Process -FilePath "C:\Windows\Temp\HpModule.exe" -Wait -ArgumentList "/silent /norestart"
+    Get-Command -Module "*HP*"
+    Start-Process -FilePath "https://developers.hp.com/hp-client-management/doc/client-management-script-library"
+    Remove-Item -Path "C:\Windows\Temp\HpModule.exe" -Force
+}
+Get-HpModule
+
+Exit
