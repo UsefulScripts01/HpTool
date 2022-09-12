b8d153b9e94bbd1779192575c88984b697439a2b
Dawid Prowadzisz <dawid.prowadzisz@gmail.com>
2022-09-12 22:23:40 +0200

    hp

--- LanWlanAutoSwitching.ps1
+++ LanWlanAutoSwitching.ps1
@@ -16,14 +16,17 @@ function Enable-AutoSwitching {
 
     $Bios = (Get-ComputerInfo).BiosManufacturer
     if ($Bios -eq "HP") {
-        Invoke-WebRequest -Uri "https://hpia.hpcloud.hp.com/downloads/cmsl/hp-cmsl-1.6.7.exe" -OutFile "C:\Windows\Temp\HpModule.exe"
-        Start-Process -FilePath "C:\Windows\Temp\HpModule.exe" -Wait -ArgumentList "/verysilent /norestart"
-        Set-HPBIOSSettingValue -Name "LAN / WLAN Auto Switching" -Value "Enable"
-        Start-Process -FilePath "C:\Program Files\WindowsPowerShell\HP.CMSL.UninstallerData\unins000.exe" -Wait -ArgumentList "/verysilent /norestart"
-        Remove-Item -Path "C:\Windows\Temp\HpModule.exe" -Force
+
+        $CmslPath = Test-Path -Path "C:\Program Files\WindowsPowerShell\HP.CMSL.UninstallerData"
+        if ($CmslPath -notmatch "True") {
+            Invoke-WebRequest -Uri "https://hpia.hpcloud.hp.com/downloads/cmsl/hp-cmsl-1.6.7.exe" -OutFile "C:\Windows\Temp\HpModule.exe"
+            Start-Process -FilePath "C:\Windows\Temp\HpModule.exe" -Wait -ArgumentList "/verysilent /norestart"
+            Remove-Item -Path "C:\Windows\Temp\HpModule.exe" -Force
+            #Start-Process -FilePath "https://developers.hp.com/hp-client-management/doc/client-management-script-library"
+        }
+        Set-HPBIOSSettingValue -Name "LAN / WLAN Auto Switching" -Value "Enable"   
     }
-}
 
-Enable-AutoSwitching
+    Enable-AutoSwitching
 
-Exit
+    Exit
