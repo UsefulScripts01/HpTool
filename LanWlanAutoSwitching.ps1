3f67ca5080f4b29121b5dbe4387be9d4161ceaeb
Dawid Prowadzisz <dawid.prowadzisz@gmail.com>
2022-09-12 22:38:11 +0200

    hp

--- LanWlanAutoSwitching.ps1
+++ LanWlanAutoSwitching.ps1
@@ -1,21 +1,22 @@
-<#
-    .SYNOPSIS
-        Enable LAN / WLAN Auto Switching
-
-    .DESCRIPTION
-        This script Enable LAN / WLAN Auto Switching in bios.
-
-    .NOTES
-        
-    .LINK
-        https://github.com/UsefulScripts01/HpModule
-    #>
-    
+function Enable-AutoSwitching {
+    <#
+	    .SYNOPSIS
+	        Enable LAN / WLAN Auto Switching
+	
+	    .DESCRIPTION
+	        This script Enable LAN / WLAN Auto Switching in bios.
+	
+	    .NOTES
+	        
+	    .LINK
+	        https://github.com/UsefulScripts01/HpModule
+	    #>
+	    
     $progressPreference = "SilentlyContinue"
-
+	
     $Bios = (Get-ComputerInfo).BiosManufacturer
     if ($Bios -eq "HP") {
-
+	
         $CmslPath = Test-Path -Path "C:\Program Files\WindowsPowerShell\HP.CMSL.UninstallerData"
         if ($CmslPath -notmatch "True") {
             Invoke-WebRequest -Uri "https://hpia.hpcloud.hp.com/downloads/cmsl/hp-cmsl-1.6.7.exe" -OutFile "C:\Windows\Temp\HpModule.exe"
@@ -25,5 +26,7 @@
         }
         Set-HPBIOSSettingValue -Name "LAN / WLAN Auto Switching" -Value "Enable"   
     }
+}
+Enable-AutoSwitching
 
-    Exit
+Exit
