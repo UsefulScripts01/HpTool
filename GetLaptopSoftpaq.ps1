883f55a761623f86fe484f7fbb6a5168fb6ec935
Dawid Prowadzisz <dawid.prowadzisz@gmail.com>
2022-11-26 23:29:11 +0100

    split file

--- GetLaptopSoftpaq.ps1
+++ GetLaptopSoftpaq.ps1
@@ -1,13 +1,3 @@
-<#
-    .SYNOPSIS
-        
-    .DESCRIPTION
-        
-    .NOTES
-
-    .LINK
-        https://github.com/UsefulScripts01/HpModule
-#>
 
 function Get-HpModule {
     $HpModule = (Get-Module -All).Name
@@ -29,11 +19,33 @@ function Get-HpModule {
     }
 }
 
+function Get-LaptopSoftpaq {
+    $CmslPath = Test-Path -Path "C:\Program Files\WindowsPowerShell\HP.CMSL.UninstallerData"
+    if ($CmslPath -notmatch "True") {
+        Invoke-WebRequest -Uri "https://hpia.hpcloud.hp.com/downloads/cmsl/hp-cmsl-1.6.7.exe" -OutFile "C:\Windows\Temp\HpModule.exe"
+        Start-Process -FilePath "C:\Windows\Temp\HpModule.exe" -Wait -ArgumentList "/verysilent /norestart"
+        Remove-Item -Path "C:\Windows\Temp\HpModule.exe" -Force
+        #Start-Process -FilePath "https://developers.hp.com/hp-client-management/doc/client-management-script-library"
+    }
+    
+    $TestPath = Test-Path -Path "C:\SOFTPAQ"
+    if ($TestPath -match "False") {
+        New-Item -Name "SOFTPAQ" -ItemType Directory -Path "C:\" -Force
+    }
+    $OsVer = (Get-ComputerInfo).OSDisplayVersion
+    $CsModel = (Get-ComputerInfo).CsModel
+    New-Item -Name $CsModel -ItemType Directory -Path "C:\SOFTPAQ\" -Force
+    $Path = "C:\SOFTPAQ\$CsModel\"
+    Invoke-Item -Path $Path
+    New-HPDriverPack -OSVer $OsVer -Path $Path -RemoveOlder -Overwrite    
+}
+
 $progressPreference = "SilentlyContinue"
 
 $Bios = (Get-CimInstance -ClassName win32_computersystem).Manufacturer
 if (($Bios -match "HP") -or ($Bios -match "Microsoft")) {
     Get-HpModule
+    Get-LaptopSoftpaq
 }
 else {
     Clear-Host
--- UpdateBios.ps1
+++ UpdateBios.ps1
@@ -1,14 +1,3 @@
-<#
-    .SYNOPSIS
-        
-    .DESCRIPTION
-        
-    .NOTES
-
-    .LINK
-        https://github.com/UsefulScripts01/HpModule
-#>
-
 function Get-HpModule {
     $HpModule = (Get-Module -All).Name
     if ($HpModule -notcontains "HPCMSL") {
@@ -29,11 +18,40 @@ function Get-HpModule {
     }
 }
 
+function Update-Bios {
+    $HpModule = (Get-Module -All).Name
+    if ($HpModule -notcontains "HPCMSL") {
+        Install-PackageProvider -Name NuGet -Force
+        Install-Module PowerShellGet -AllowClobber -Force
+        Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
+        
+        Start-Process -FilePath "powershell" -Wait -WindowStyle Hidden {
+            Install-Module -Name HPCMSL -Force -AcceptLicense
+        }
+        
+        Get-Module -All -Name "HP*" | Import-Module -Global
+        Get-Module -All -Name "HP*" | Format-Table
+        
+        Write-Host "`n"
+        Write-Host "REF: https://developers.hp.com/hp-client-management/doc/client-management-script-library (Ctrl + Link to open website)"
+        Write-Host "`n"
+    }
+
+    $VolumeStatus = (Get-BitLockerVolume).VolumeStatus
+    if ($VolumeStatus -ne "FullyDecrypted") {
+        Suspend-BitLocker -MountPoint "C:" -RebootCount 1
+    }
+
+    Get-HPBIOSUpdates
+    Get-HPBIOSUpdates -Flash -Force
+}
+
 $progressPreference = "SilentlyContinue"
 
 $Bios = (Get-CimInstance -ClassName win32_computersystem).Manufacturer
 if (($Bios -match "HP") -or ($Bios -match "Microsoft")) {
     Get-HpModule
+    Update-Bios
 }
 else {
     Clear-Host
