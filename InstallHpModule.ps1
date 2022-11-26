38295eb42efd3f9a05f5648eaaebbffda96b0ac6
Dawid Prowadzisz <dawid.prowadzisz@gmail.com>
2022-11-26 20:04:19 +0100

    Update InstallHpModule.ps1

--- InstallHpModule.ps1
+++ InstallHpModule.ps1
@@ -1,3 +1,4 @@
+
 function Get-HpModule {
     #Set-ExecutionPolicy Bypass -Force
     Install-PackageProvider -Name NuGet -Force
@@ -7,38 +8,3 @@ function Get-HpModule {
 }
 Get-HpModule
 Start-Process -FilePath "https://developers.hp.com/hp-client-management/doc/client-management-script-library"
-
-
-<#
-function Get-HpCmsl {
-    <#
-    .SYNOPSIS
-        Client Management Script Library
-
-    .DESCRIPTION
-        This script downloads and installs the "HP Client Management Script Library".
-
-    .NOTES
-        
-    .LINK
-        https://github.com/UsefulScripts01/HpModule
-    #>
-    $progressPreference = "SilentlyContinue"
-
-    $Bios = (Get-ComputerInfo).BiosManufacturer
-    if ($Bios -contains "HP" -or "Microsoft") {
-
-        $CmslPath = Test-Path -Path "C:\Program Files\WindowsPowerShell\HP.CMSL.UninstallerData"
-        if ($CmslPath -eq "False") {
-            Invoke-WebRequest -Uri "https://hpia.hpcloud.hp.com/downloads/cmsl/hp-cmsl-1.6.7.exe" -OutFile "C:\Windows\Temp\HpModule.exe"
-            Start-Process -FilePath "C:\Windows\Temp\HpModule.exe" -Wait -ArgumentList "/verysilent /norestart"
-            Remove-Item -Path "C:\Windows\Temp\HpModule.exe" -Force
-            Start-Process -FilePath "https://developers.hp.com/hp-client-management/doc/client-management-script-library"
-        }
-        Invoke-WebRequest -Uri "https://raw.githubusercontent.com/UsefulScripts01/HpModule/main/HpModule.psm1" -OutFile "~\Documents\WindowsPowerShell\HpModule.psm1"
-        Import-Module -Name "~\Documents\WindowsPowerShell\HpModule.psm1" -Global -Force
-    }
-}
-Get-HpCmsl
-Get-Command -Module HpModule
-#>
