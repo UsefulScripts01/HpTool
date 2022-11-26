8f2523adfa2500e5d8793542cd9c13a54c796d14
Dawid Prowadzisz <dawid.prowadzisz@gmail.com>
2022-11-26 20:00:47 +0100

    module

--- InstallHpModule.ps1
+++ InstallHpModule.ps1
@@ -1,3 +1,15 @@
+function Get-HpModule {
+    #Set-ExecutionPolicy Bypass -Force
+    Install-PackageProvider -Name NuGet -Force
+    Install-Module PowerShellGet -AllowClobber -Force
+    Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
+    Install-Module -Name HPCMSL -AcceptLicense -Force
+}
+Get-HpModule
+Start-Process -FilePath "https://developers.hp.com/hp-client-management/doc/client-management-script-library"
+
+
+<#
 function Get-HpCmsl {
     <#
     .SYNOPSIS
@@ -29,3 +41,4 @@ function Get-HpCmsl {
 }
 Get-HpCmsl
 Get-Command -Module HpModule
+#>
