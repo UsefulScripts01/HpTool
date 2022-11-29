1be48c341f87bbe896fd75f0f9b8fe0bf35d70d9
Dawid Prowadzisz <dawid.prowadzisz@gmail.com>
2022-11-29 12:09:08 +0100

    Update HpModule.ps1

--- HpModule.ps1
+++ HpModule.ps1
@@ -11,8 +11,7 @@
 
 
 function Get-HpModule {
-    $ModuleList = (Get-Module -Name HPCMSL -ListAvailable).Name
-    if ($ModuleList -notmatch "HPCMS") {
+    if (!(Get-Module -ListAvailable).Name.Contains('HPCMSL')) {
         Invoke-WebRequest -Uri "https://hpia.hpcloud.hp.com/downloads/cmsl/hp-cmsl-1.6.8.exe" -OutFile "C:\Windows\Temp\hpcmsl.exe"
         Start-Process -FilePath "C:\Windows\Temp\hpcmsl.exe" -Wait -ArgumentList "/VERYSILENT"
     }
