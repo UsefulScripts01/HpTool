485a549b32b209858fad3acb4fd86e115f55cc0d
Dawid Prowadzisz <dawid.prowadzisz@gmail.com>
2022-12-07 15:25:03 +0100

    Update HpTool.ps1

--- HpTool.ps1
+++ HpTool.ps1
@@ -13,7 +13,7 @@
 function Get-HpModule {
 
     $HpPath = Test-Path -Path "C:\Program Files\WindowsPowerShell\Modules\HPCMSL"
-    if ($HpPath -eq "False") {
+    if ($HpPath -match "False") {
         Invoke-WebRequest -Uri "https://hpia.hpcloud.hp.com/downloads/cmsl/hp-cmsl-1.6.8.exe" -OutFile "C:\Windows\Temp\hpcmsl.exe"
         Start-Process -FilePath "C:\Windows\Temp\hpcmsl.exe" -Wait -ArgumentList "/VERYSILENT"
         Start-Sleep -Seconds 5
