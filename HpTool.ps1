253a1ac5d5bba432f70a46b14f37135503587544
Dawid Prowadzisz <dawid.prowadzisz@gmail.com>
2022-12-13 00:10:59 +0100

    Update HpTool.ps1

--- HpTool.ps1
+++ HpTool.ps1
@@ -279,6 +279,18 @@ function Enable-SelectEncryption {
     }
 }
 
+# Enable LAN / WLAN Auto Switching
+function Enable-AutoSwitching {
+    $Bios = (Get-ComputerInfo).BiosManufacturer
+    if ($Bios -eq "HP") {
+        Invoke-WebRequest -Uri "https://hpia.hpcloud.hp.com/downloads/cmsl/hp-cmsl-1.6.7.exe" -OutFile "C:\Windows\Temp\HpModule.exe"
+        Start-Process -FilePath "C:\Windows\Temp\HpModule.exe" -Wait -ArgumentList "/verysilent /norestart"
+        Set-HPBIOSSettingValue -Name "LAN / WLAN Auto Switching" -Value "Enable"
+        Start-Process -FilePath "C:\Program Files\WindowsPowerShell\HP.CMSL.UninstallerData\unins000.exe" -Wait -ArgumentList "/verysilent /norestart"
+        Remove-Item -Path "C:\Windows\Temp\HpModule.exe" -Force
+    }
+}
+
 # Save error log
 function Save-ErrorLog {
     if (!$Error.Count.Equals(0)) {
